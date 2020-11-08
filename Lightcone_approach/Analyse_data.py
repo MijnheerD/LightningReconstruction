09df91f7a6493592098707351cc07bd4214ff74c
Mitja <34456399+MijnheerD@users.noreply.github.com>
2020-11-08 11:14:20 +0100

    Constructed Stepper class

--- Lightcone_approach/Analyse_data.py
+++ Lightcone_approach/Analyse_data.py
@@ -1,2 +1,91 @@
 import numpy as np
-data=np.genfromtxt("output.txt", delimiter=",")
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from scipy.constants import c
+from sortedcontainers import SortedList, SortedSet
+
+data = np.genfromtxt("data_test.txt", delimiter=",")
+x = data[:, 0]
+y = data[:, 1]
+z = data[:, 2]
+t = data[:, 3]
+
+
+# Make sure data is sorted by time
+
+
+def in_spacetime_interval(p):
+    x0, y0, z0, t0 = p
+    # c = 10**7  # Use speed of lightning propagation
+    ds = -c ** 2 * (t - t0) ** 2 + (x - x0) ** 2 + (y - y0) ** 2 + (z - z0) ** 2
+    return ds <= 0
+
+
+def add_weight(p1, p2):
+    x1, y1, z1, t1 = p1
+    x2, y2, z2, t2 = p2
+    w = (x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2
+    return w
+
+
+POINTS = len(t)
+POOL = SortedList([np.random.randint(0, POINTS + 1)])  # Start with a random seed
+RANGE = np.array(range(POINTS))
+
+update = 1
+while update:
+    print(f'Still looping, I have already selected {len(POOL)} points')
+    update = 0
+    points = SortedSet()  # Points reachable from pool of seeds
+    comparison = {}
+
+    for el in [POOL[0], POOL[-1]]:
+        selection = in_spacetime_interval((x[el], y[el], z[el], t[el]))
+        in_cone = RANGE[selection]
+
+        if len(in_cone) != 0:
+            update = 1
+
+        for point in in_cone:
+            if point not in POOL:
+                weight = add_weight((x[el], y[el], z[el], t[el]), (x[point], y[point], z[point], t[point]))
+                if point in comparison:
+                    old = comparison[point]
+                else:
+                    old = 1e8
+
+                if weight < old:
+                    comparison[point] = weight
+
+    seed_add = min(comparison, key=comparison.__getitem__)
+    POOL.add(seed_add)
+
+SELECTION = np.array(POOL)
+
+fig = plt.figure(1, figsize=(20, 10))
+ax = fig.add_subplot(121, projection='3d')
+ax2 = fig.add_subplot(122, projection='3d')
+
+ax.scatter(x, y, z, marker='^', c='b')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_title('Original data')
+
+ax2.scatter(x[SELECTION], y[SELECTION], z[SELECTION], marker='o', c='r')
+ax2.set_xlabel('X')
+ax2.set_ylabel('Y')
+ax2.set_zlabel('Z')
+ax2.set_title('Data selected by lightcone algorithm')
+
+fig2 = plt.figure(2, figsize=(15, 15))
+ax3 = fig2.add_subplot(111, projection='3d')
+
+ax3.scatter(x, y, z, marker='^', c='b')
+ax3.scatter(x[SELECTION], y[SELECTION], z[SELECTION], marker='o', c='r')
+ax3.set_xlabel('X')
+ax3.set_ylabel('Y')
+ax3.set_zlabel('Z')
+ax3.set_title('Overlap of the selection with the original data')
+
+plt.show()
---
+++ Lightcone_approach/Lightcone_search.py
@@ -0,0 +1,87 @@
+from Lightcone_approach.LinAlg import angle_between
+from sortedcontainers import SortedList
+import numpy as np
+
+
+class Stepper:
+    def __init__(self, x: np.ndarray, y: np.ndarray, z: np.ndarray, t: np.ndarray, seed, weights=(1, 0), t_cut=1e-9):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.t = t
+        self.pool = SortedList([seed])
+        self.distance_weight, self.vel_weight = weights
+        self.time_cutoff = t_cut
+
+    def in_lightcone(self, p: int):
+        """
+        Check which points are within the lightcone of the current selected point.
+        :param p: Index in the data of the current selected point.
+        :return: List of booleans pointing if points are in the lightcone or not.
+        """
+        x0, y0, z0, t0 = self.x[p], self.y[p], self.z[p], self.t[p]
+
+        # Points must be close enough in time to be eligible
+        x = self.x[(t0 - self.time_cutoff) <= self.t <= (t0 + self.time_cutoff)]
+        y = self.y[(t0 - self.time_cutoff) <= self.t <= (t0 + self.time_cutoff)]
+        z = self.z[(t0 - self.time_cutoff) <= self.t <= (t0 + self.time_cutoff)]
+        t = self.t[(t0 - self.time_cutoff) <= self.t <= (t0 + self.time_cutoff)]
+
+        # Use speed of lightning propagation
+        c = 10 ** 7
+
+        # Calculate spacetime interval
+        ds = -c ** 2 * (t - t0) ** 2 + (x - x0) ** 2 + (y - y0) ** 2 + (z - z0) ** 2
+
+        return ds <= 0
+
+    def _distance_pair(self, p1, p2):
+        """
+        Calculate the Euclidean distance between 2 points.
+        :param p1: Index in the data of the first selected point.
+        :param p2: Index in the data of the second selected point.
+        :return: Euclidean distance.
+        """
+        x1, y1, z1 = self.x[p1], self.y[p1], self.z[p1]
+        x2, y2, z2 = self.x[p2], self.y[p2], self.z[p2]
+
+        return (x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2
+
+    def _velocity_penalty(self, v1, v2):
+        """
+        Gives a measure from how much two velocity vectors are aligned.
+        :param v1: First velocity vector, tuple or list of 2 indices.
+        :param v2: Second velocity vector, same format as first.
+        :return: Number between 0 and 1. Close to 0 means that the two vector are (anti-)aligned.
+        """
+        p1, p2 = v1
+        vel1 = [self.x[p2] - self.x[p1], self.y[p2] - self.y[p1], self.z[p2] - self.z[p1]]
+        p1, p2 = v2
+        vel2 = [self.x[p2] - self.x[p1], self.y[p2] - self.y[p1], self.z[p2] - self.z[p1]]
+        angle = angle_between(vel1, vel2)
+
+        return np.sin(angle)
+
+    def find_next(self):
+        """
+        Finds next points at both begin and tail of the current researched branch.
+        :return:
+        """
+        for index in [-1, 0]:
+            element = self.pool[index]
+            values = {}
+            possible_points = set()
+
+            select = self.in_lightcone(element)
+            indices = np.array(range(len(self.t)))
+            possible_points.add(indices[select])
+
+            for point in possible_points:
+                if point not in self.pool:
+                    d = self._distance_pair(element, point)
+                    v = self._velocity_penalty((self.pool[index + (-1)**index], element), (element, point))
+
+                    values[point] = self.distance_weight * d + self.vel_weight * v
+
+            seed_add = min(values, key=values.__getitem__)
+            self.pool.add(seed_add)
