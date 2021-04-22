cefc76f72d3a01dcfd70d3859b5271afc2779f1c
MijnheerD <mitja.desmet2@gmail.com>
2021-04-22 15:33:04 +0200

    Plotting final voxel configurations with more information

--- Mesh_approach/Test_graphviz.py
+++ Mesh_approach/Test_graphviz.py
@@ -1,5 +1,6 @@
 from numpy import genfromtxt
 from graphviz import Graph
+from matplotlib import cm
 from Mesh_approach.LightningAnalyzer import Analyzer
 
 data = genfromtxt("../Data/data.txt", delimiter=",")
@@ -26,21 +27,36 @@ tree.octree.refine(min_side=tree.min_voxel_size, max_side=tree.max_voxel_size)
 
 graph = Graph(comment='The voxel structure', filename='Graph_subset1', format='png')
 voxels = tree.octree.find_leaves(tree.octree.root)
+max_score = 1000
+seismic = cm.get_cmap("seismic")
 
 for counter in range(len(voxels)):
-    if voxels[counter].label != 0:
-        graph.node('voxel'+str(counter), label=None, shape='point')
+    size = len(voxels[counter].contents)
+    graph.node('voxel'+str(counter), label=None, shape='point', fixedsize='true', width=str(0.05*size))
 for counter in range(len(voxels)):
     connection = False
     nn = voxels[counter].neighbours
-    if len(nn) > 3:
-        top_nn = dict(sorted(nn.items(), key=lambda k: k[1][1])[:3])
-    else:
-        top_nn = nn
+    # if len(nn) > 3:
+    #     top_nn = dict(sorted(nn.items(), key=lambda k: k[1][1])[:3])
+    # else:
+    top_nn = nn
     for neighbour in top_nn:
         try:
             pointer = voxels.index(neighbour)
-            graph.edge('voxel' + str(counter), 'voxel' + str(pointer))
+            ninfo = top_nn[neighbour]
+            if ninfo[0] == 'face':
+                linestyle = 'solid'
+            elif ninfo[0] == 'edge':
+                linestyle = 'dashed'
+            else:
+                linestyle = 'dotted'
+
+            if ninfo[1] > max_score:
+                max_score = ninfo[1] + 1
+            norm = ninfo[1] / max_score
+            color = seismic(norm)
+            graph.edge('voxel' + str(counter), 'voxel' + str(pointer), style=linestyle,
+                       color=f"{color[0]:.3f} {color[1]:.3f} {color[2]:.3f}")
             connection = True
         except ValueError:
             pass
