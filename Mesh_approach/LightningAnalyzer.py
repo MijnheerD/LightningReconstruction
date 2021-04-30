66af1c20332c8b070e3b4ef023f95663e1032dd1
MijnheerD <mitja.desmet2@gmail.com>
2021-04-30 12:16:36 +0200

    Working version of minimal spanning tree for voxel graph and data points

--- Mesh_approach/LightningAnalyzer.py
+++ Mesh_approach/LightningAnalyzer.py
@@ -1,8 +1,12 @@
-from numpy import concatenate, zeros, where, sum
+"""
+TODO: incorporate lonely voxels -> do not use them in mst
+"""
+
+from numpy import concatenate, zeros, where, sum, setdiff1d
 from scipy.sparse import lil_matrix, diags
 from scipy.sparse.csgraph import minimum_spanning_tree
 from Analyzer_template import LightningReconstructor, ListNode
-from Mesh_approach.Mesh_octree import Octree, Voxel
+from Mesh_approach.Mesh_octree import Octree
 
 
 def dfc(sym_graph, current_vertex, visited=None):
@@ -54,64 +58,61 @@ class Analyzer(LightningReconstructor):
     def find_begin_voxel(self):
         endpoints = where(sum(self.connections, axis=1) == 1)
         minimum = 1000
-        minimum_leaf = None
+        minimum_leaf_ind = None
         for ind in endpoints[0]:
             if ind in self.removed:
                 continue
             leaf = self.voxels[ind]
             if min(leaf.contents) < minimum:
                 minimum = min(leaf.contents)
-                minimum_leaf = ind
+                minimum_leaf_ind = ind
                 if minimum == 0:
                     break
-        return minimum_leaf
+        return minimum_leaf_ind
 
     def find_next_branch(self, graph, start, previous=None):
         if previous is None:
             previous = start
+        next_voxel = start
 
-        neighbours = graph[previous].nonzero()[1]
-        neighbours = neighbours[neighbours != previous]
-        if len(neighbours) == 0:
-            raise Exception("Cannot find connecting voxel after start")
-        else:
-            next_voxel = neighbours[0]
-            counter = 1
-            while next_voxel in self.removed:
-                next_voxel = neighbours[counter]
-                counter += 1
-                if counter > len(neighbours):
-                    raise Exception("Cannot find voxel after start")
-
+        # Collect the indices of the data points inside the branch
         branch = []
-        branch.extend(self.voxels[start].contents)
         while next_voxel not in self.branching_points:
             branch.extend(self.voxels[next_voxel].contents)
             neighbours = graph[next_voxel].nonzero()[1]
             if sum(self.connections[next_voxel]) > 2:
+                # If the voxel has more connections than anticipated, absorb the least important ones
                 for nn in neighbours:
                     if sum(self.connections[nn]) == 1:
-                        # Absorb dangling voxels
+                        # Sum == 1 implies a voxel which had a trivial connection, but absorbed it
                         self.voxels[next_voxel].contents = concatenate([self.voxels[next_voxel].contents,
                                                                         self.voxels[nn].contents])
                         neighbours = neighbours[neighbours != nn]
                 if len(neighbours) > 2:
                     raise Exception("Cannot handle this situation: too many neighbours")
             elif sum(self.connections[next_voxel]) == 1:
-                break
+                if previous != next_voxel:
+                    break
             for nn in neighbours:
-                if nn is not previous:
+                if nn != previous and nn not in self.removed:
                     previous = next_voxel
                     next_voxel = nn
                     break
+                if nn == neighbours[-1]:
+                    raise Exception("No next voxel to find here!")
 
         # Put all the data points of the branching point in the current branch
         # TO BE UPDATED WITH PROPER DISTRIBUTION FUNCTION
         branch.extend(self.voxels[next_voxel].contents)
+
+        # If we are at a branching point, find the next points to start searching from
         neighbours = graph[next_voxel].nonzero()[1]
         neighbours = neighbours[neighbours != previous]
 
-        return branch, next_voxel, neighbours
+        # Make sure to not pass on removed voxels
+        return_neighbours = setdiff1d(neighbours, self.removed)
+
+        return branch, next_voxel, return_neighbours
 
     def find_branches(self, sym, start_voxel, branch: ListNode, previous=None):
         if self.counter >= self.max_branch:
@@ -124,6 +125,8 @@ class Analyzer(LightningReconstructor):
         if len(next_points) > 0:
             self.find_branches(sym, next_points[0], new_node, previous=end_voxel)
             self.find_branches(sym, next_points[1], new_node, previous=end_voxel)
+        else:
+            return
 
     def make_mst(self):
         # Make the adjacency matrix of the voxel configuration/graph
@@ -192,7 +195,7 @@ class Analyzer(LightningReconstructor):
         for lone in lonely:
             try:
                 pointer = self.voxels.index(lone)
-                self.lonely.append(pointer)
+                self.lonely.extend(self.voxels[pointer].contents)
             except ValueError:
                 pass
 
